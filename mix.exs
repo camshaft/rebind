ce5919a2666df26f2f6014807a17a97c2813d98d
camshaft <bytheway.cameron@gmail.com>
2015-05-20 17:28:25 -0600

    release 0.1.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Rebind.Mixfile do
 
   def project do
     [app: :rebind,
-     version: "0.1.1",
+     version: "0.1.3",
      elixir: "~> 1.0",
      description: "rebind parse transform for erlang",
      package: package,
